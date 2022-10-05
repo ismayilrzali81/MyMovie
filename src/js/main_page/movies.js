319d0ba29ac1a2430c357bc2b15a11cbd8cdcadc
elgiz20002000 <92743270+elgiz20002000@users.noreply.github.com>
2022-10-05 12:59:35 +0400

    commit

--- src/js/main_page/movies.js
+++ src/js/main_page/movies.js
@@ -63,169 +63,169 @@ export const movies = async (url_1 , place_1 , url_2 , place_2 , place_3 , place
    most_popular(url_1 , place_1 , true , true , response) 
 
 // // // // // //     // new Arrival
-//      new_arrival(url_2 , place_2)
+     new_arrival(url_2 , place_2)
 
  
 
-// // // // // // // exclusive
-//     let movies_info = []
-//     const movies = document.querySelector(`${place_3} div.movie_list`)
-//         for(let i = 0 ; i < 5  ; i++) {
-//             let randomMovie = response.data.items[i]
+// // // // // // exclusive
+    let movies_info = []
+    const movies = document.querySelector(`${place_3} div.movie_list`)
+        for(let i = 0 ; i < 5  ; i++) {
+            let randomMovie = response.data.items[i]
 
-//             const data = await axios.get(`https://imdb-api.com/en/API/Trailer/k_7xs694cx/${randomMovie.id}`)
+            const data = await axios.get(`https://imdb-api.com/en/API/Trailer/k_7xs694cx/${randomMovie.id}`)
 
-//                 movies_info.push({
-//                     videoId:data.data.videoId
-//                 }) 
+                movies_info.push({
+                    videoId:data.data.videoId
+                }) 
                 
-//                 if(data.data.thumbnailUrl) {
-//                     movies.innerHTML += 
-//                     `<div class="movie_list_item" data-index=${i} >
-//                     <div class="photo_wrapper">
-//                         <img  alt="movie photo" loading="lazy" src="${data.data.thumbnailUrl}">
-//                     </div>
-//                     <div class="title">${data.data.fullTitle} Trailer </div>
-//                     </div>`
-//                 } else {
-//                     continue;
-//                 }
-//             }
+                if(data.data.thumbnailUrl) {
+                    movies.innerHTML += 
+                    `<div class="movie_list_item" data-index=${i} >
+                    <div class="photo_wrapper">
+                        <img  alt="movie photo" loading="lazy" src="${data.data.thumbnailUrl}">
+                    </div>
+                    <div class="title">${data.data.fullTitle} Trailer </div>
+                    </div>`
+                } else {
+                    continue;
+                }
+            }
 
-//         document.querySelector(place_3).addEventListener('click' , (e) => {
-//             let target = e.target
+        document.querySelector(place_3).addEventListener('click' , (e) => {
+            let target = e.target
             
-//                 if(target.classList.contains('movie_list_item'))  {
-//                         document.querySelector('.video_wrapper').classList.toggle('active')
+                if(target.classList.contains('movie_list_item'))  {
+                        document.querySelector('.video_wrapper').classList.toggle('active')
 
-//                             if(document.querySelector('#player')) {
-//                             document.querySelector('#player').remove() 
-//                             let player = document.createElement('div')
-//                             player.id = 'player'
-//                             document.querySelector('.video_wrapper').append(player)
-//                         }
+                            if(document.querySelector('#player')) {
+                            document.querySelector('#player').remove() 
+                            let player = document.createElement('div')
+                            player.id = 'player'
+                            document.querySelector('.video_wrapper').append(player)
+                        }
 
-//                         new YT.Player('player', {
-//                             height: '360',
-//                             width: '640',
-//                             videoId: movies_info[target.dataset.index].videoId
-//                             });
-//                 }
+                        new YT.Player('player', {
+                            height: '360',
+                            width: '640',
+                            videoId: movies_info[target.dataset.index].videoId
+                            });
+                }
                 
-//                 if(target.parentElement.classList.contains('movie_list_item')) {
-//                         document.querySelector('.video_wrapper').classList.toggle('active')
+                if(target.parentElement.classList.contains('movie_list_item')) {
+                        document.querySelector('.video_wrapper').classList.toggle('active')
 
-//                         if(document.querySelector('#player')) {
-//                             document.querySelector('#player').remove() 
-//                             let player = document.createElement('div')
-//                             player.id = 'player'
-//                             document.querySelector('.video_wrapper').append(player)
-//                         }
+                        if(document.querySelector('#player')) {
+                            document.querySelector('#player').remove() 
+                            let player = document.createElement('div')
+                            player.id = 'player'
+                            document.querySelector('.video_wrapper').append(player)
+                        }
 
-//                             new YT.Player('player', {
-//                             height: '360',
-//                             width: '640',
-//                             videoId: movies_info[target.parentElement.dataset.index].videoId
-//                             });
-//                 }
+                            new YT.Player('player', {
+                            height: '360',
+                            width: '640',
+                            videoId: movies_info[target.parentElement.dataset.index].videoId
+                            });
+                }
                 
-//                 if(target.parentElement.parentElement.classList.contains('movie_list_item') && !(target.classList.contains('favorite_toggle_btn'))) {
-//                         document.querySelector('.video_wrapper').classList.toggle('active')
+                if(target.parentElement.parentElement.classList.contains('movie_list_item') && !(target.classList.contains('favorite_toggle_btn'))) {
+                        document.querySelector('.video_wrapper').classList.toggle('active')
 
 
 
-//                             if(document.querySelector('#player')) {
-//                             document.querySelector('#player').remove() 
-//                             let player = document.createElement('div')
-//                             player.id = 'player'
-//                             document.querySelector('.video_wrapper').append(player)
-//                         }
+                            if(document.querySelector('#player')) {
+                            document.querySelector('#player').remove() 
+                            let player = document.createElement('div')
+                            player.id = 'player'
+                            document.querySelector('.video_wrapper').append(player)
+                        }
 
-//                         new YT.Player('player', {
-//                             height: '360',
-//                             width: '640',
-//                             videoId: movies_info[target.parentElement.parentElement.dataset.index].videoId
-//                             });
-//                 }
-//         })        
+                        new YT.Player('player', {
+                            height: '360',
+                            width: '640',
+                            videoId: movies_info[target.parentElement.parentElement.dataset.index].videoId
+                            });
+                }
+        })        
 
 
        
 
 
 
-// // // //     // main_screen
+// // //     // main_screen
    
 
-//     axios.get(url_1).then(async(response) => {
+    axios.get(url_1).then(async(response) => {
       
-//         let movies_info = []
+        let movies_info = []
         
         
-//         for(let i = 0 ; i < 5 ; i++) {
-//             let randomMovie = response.data.items[i]
+        for(let i = 0 ; i < 5 ; i++) {
+            let randomMovie = response.data.items[i]
 
-//             const data = await  axios.get(`https://imdb-api.com/en/API/YouTubeTrailer/k_7xs694cx/${randomMovie.id}`)
-//             let res = await axios.get(`http://www.omdbapi.com/?apikey=8d9cfe3d&i=${randomMovie.id}`) ,
-//             poster = await  (await axios.get(`https://www.myapifilms.com/imdb/idIMDB?idIMDB=${randomMovie.id}&token=f183f32a-f21f-435a-81ca-2778aad3a1fe`)).data.data.movies[0].urlPoster
+            const data = await  axios.get(`https://imdb-api.com/en/API/YouTubeTrailer/k_7xs694cx/${randomMovie.id}`)
+            let res = await axios.get(`http://www.omdbapi.com/?apikey=8d9cfe3d&i=${randomMovie.id}`) ,
+            poster = await  (await axios.get(`https://www.myapifilms.com/imdb/idIMDB?idIMDB=${randomMovie.id}&token=f183f32a-f21f-435a-81ca-2778aad3a1fe`)).data.data.movies[0].urlPoster
 
-//                 movies_info.push({
-//                     title:randomMovie.title ,
-//                     imdbRatings: (res.data.imdbRating && res.data.imdbRating !== 'N/A' ) ? res.data.imdbRating : 0 ,
-//                     poster: poster ,
-//                     video: data.data.videoId ,
-//                     content: res.data.Plot
+                movies_info.push({
+                    title:randomMovie.title ,
+                    imdbRatings: (res.data.imdbRating && res.data.imdbRating !== 'N/A' ) ? res.data.imdbRating : 0 ,
+                    poster: poster ,
+                    video: data.data.videoId ,
+                    content: res.data.Plot
 
-//                 })
-//         }
+                })
+        }
 
-//         return movies_info
+        return movies_info
         
-//     }).then((movies_info) => {
-//         const movies = document.querySelector(`${place_4} `)
+    }).then((movies_info) => {
+        const movies = document.querySelector(`${place_4} `)
 
-//         document.querySelectorAll('.movie_toggle li').forEach((item , index) => {
-//             item.addEventListener('click' , () => {
+        document.querySelectorAll('.movie_toggle li').forEach((item , index) => {
+            item.addEventListener('click' , () => {
                 
-//                 document.querySelector('.header_wrapper').style.cssText = `background: url(${movies_info[index].poster}) 
-//                 center 35% /cover no-repeat`
+                document.querySelector('.header_wrapper').style.cssText = `background: url(${movies_info[index].poster}) 
+                center 35% /cover no-repeat`
 
-//                 if(document.querySelector('iframe#player')) {
-//                     document.querySelector('iframe#player').remove() 
-//                     let player = document.createElement('div')
-//                     player.id = 'player'
-//                     document.querySelector('.video_wrapper').append(player)
-//                 } 
+                if(document.querySelector('iframe#player')) {
+                    document.querySelector('iframe#player').remove() 
+                    let player = document.createElement('div')
+                    player.id = 'player'
+                    document.querySelector('.video_wrapper').append(player)
+                } 
                 
-//                 new YT.Player('player', {
-//                 height: '360',
-//                 width: '640',
-//                 videoId: `${movies_info[index].video}`
-//                 });
+                new YT.Player('player', {
+                height: '360',
+                width: '640',
+                videoId: `${movies_info[index].video}`
+                });
 
 
-//                 movies.innerHTML = 
-//                 ` <div class="container">
-//                     <div class="title">${movies_info[index].title}</div>
-//                     <div class="score">
-//                         <div class="imdb_score"> <img loading="lazy" class="imdb"  alt="imdb">${(movies_info[index].imdbRatings  || movies_info[index].imdbRatings =='N/A')? movies_info[index].imdbRatings : 0 } / 10</div>
-//                     </div>
-//                     <div class="content">${movies_info[index].content}</div>
-//                     <button class="watch_trailer"> <img  alt="play">  Watch trailer</button>
-//                 </div>`
-//             })
-//         })
+                movies.innerHTML = 
+                ` <div class="container">
+                    <div class="title">${movies_info[index].title}</div>
+                    <div class="score">
+                        <div class="imdb_score"> <img loading="lazy" class="imdb"  alt="imdb">${(movies_info[index].imdbRatings  || movies_info[index].imdbRatings =='N/A')? movies_info[index].imdbRatings : 0 } / 10</div>
+                    </div>
+                    <div class="content">${movies_info[index].content}</div>
+                    <button class="watch_trailer"> <img  alt="play">  Watch trailer</button>
+                </div>`
+            })
+        })
 
-//         movies.innerHTML = 
-//         ` <div class="container">
-//             <div class="title">${movies_info[2].title}</div>
-//             <div class="score">
-//                 <div class="imdb_score"> <img class="imdb"  alt="imdb">${(movies_info[2].imdbRatings || movies_info[2].imdbRatings =='N/A') ? movies_info[2].imdbRatings : 0 } / 10</div>
-//             </div>
-//             <div class="content">${movies_info[2].content}</div>
-//             <button class="watch_trailer"> <img  alt="play">  Watch trailer</button>
-//         </div>`
+        movies.innerHTML = 
+        ` <div class="container">
+            <div class="title">${movies_info[2].title}</div>
+            <div class="score">
+                <div class="imdb_score"> <img class="imdb"  alt="imdb">${(movies_info[2].imdbRatings || movies_info[2].imdbRatings =='N/A') ? movies_info[2].imdbRatings : 0 } / 10</div>
+            </div>
+            <div class="content">${movies_info[2].content}</div>
+            <button class="watch_trailer"> <img  alt="play">  Watch trailer</button>
+        </div>`
 
-//         document.querySelector('.header_wrapper').style.cssText = `background: url(${movies_info[2].poster}) center 35% /cover no-repeat`
-//     })
+        document.querySelector('.header_wrapper').style.cssText = `background: url(${movies_info[2].poster}) center 35% /cover no-repeat`
+    })
 }
